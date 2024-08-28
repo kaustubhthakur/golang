5f034e5bec48d1f46cf9a9a9334756cd3f4bf3ea
Kaustubh <kaustubhthakur76@gmail.com>
2024-08-29 00:09:28 +0530

    tcp-server

--- tcp-server/main.go
+++ tcp-server/main.go
@@ -1,31 +1,75 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
+type Message struct {
+	from    string
+	payload []byte
+}
 type Server struct {
 	listenAddr string
 	ln         net.Listener
 	quitch     chan struct{}
+	msgch      chan Message
+	peerMap map[net.Addr]
 }
 
 func NewServer(listenAdrr string) *Server {
 	return &Server{
 		listenAddr: listenAdrr,
 		quitch:     make(chan struct{}),
+		msgch:      make(chan Message, 10),
 	}
 }
 
 func (s *Server) Start() error {
-	ln, err := net.Listen("tpc", s.listenAddr)
+	ln, err := net.Listen("tcp", s.listenAddr)
 	if err != nil {
 		return err
 	}
 	defer ln.Close()
 	s.ln = ln
-
+	go s.acceptLoop()
 	<-s.quitch
+	close(s.msgch)
 	return nil
 }
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			fmt.Println("accept error:", err)
+			continue
+		}
+		fmt.Println("new connection", conn.RemoteAddr())
+		go s.readLoop(conn)
+	}
+}
+func (s *Server) readLoop(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 2048)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("read error:", err)
+			continue
+		}
+		s.msgch <- Message{
+			from:    conn.RemoteAddr().String(),
+			payload: buf[:n],
+		}
+		conn.Write([]byte("thank you for your message"))
+	}
+}
 func main() {
-
+	server := NewServer(":3000")
+	go func() {
+		for msg := range server.msgch {
+			fmt.Printf("received mesage from connection (%d):%s\n", msg.from, string(msg.payload))
+		}
+	}()
+	server.Start()
 }
